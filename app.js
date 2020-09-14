c2a87f03b64413964277cac0c7bf636edc36ef0c
furkangulluu <furkangulluu@gmail.com>
2020-09-14 19:16:46 +0300

    db source change again to local json file

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const app = require('express')();
 const bodyParser = require('body-parser');
+const db = require('./db.json')
 const axios = require('axios')
+const uniqid = require('uniqid');
 var cors = require('cors')
 app.use(cors())
 
@@ -10,48 +12,26 @@ app.use(bodyParser.urlencoded({extended:true}))
 //Get data with the last in 30min
 app.get('/datas',(req,res) => {
     let currentDate = new Date();
-    const dataList= [];
-
-    axios.get("https://perfanalyticsdb.firebaseio.com/data.json")
-    .then((response) => {
-        let data=response.data;
-        
-        for (let key in data) {
-            dataList.push({ ...data[key], id : key })
-        }
-
-        let result = dataList.filter(data=>{
-            let takenDate = new Date(data.createdTime);
-            return (Math.abs(currentDate - takenDate) < 1800000)
-        })
-
-        res.status(200).send(result)
-        if(result.length>0){
-        }else{
-            res.status(404).send({message: 'Data not found'})
-        }
+    
+    let result = db.filter(data=>{
+        let takenDate = new Date(data.createdTime);
+        return (Math.abs(currentDate - takenDate) < 1800000)
     })
-    .catch((e) => {
-        res.status(404).send({message: 'Code Problem!'+e})
-    });
+    res.send(result)
 })
 
 //Post data
 app.post('/datas',(req,res) => {
-    const postData = {
-        ttfb : req.body.ttfb,
-        fcp : req.body.fcp,
-        domLoad : req.body.domLoad,
-        windowLoad : req.body.windowLoad,
-        createdTime : new Date()
+    const newData={
+        id: uniqid(),
+        ttfb: req.body.ttfb,
+        fpc: req.body.fpc,
+        domLoad: req.body.domLoad,
+        windowLoad: req.body.windowLoad,
+        createdTime: new Date()
     }
-    axios.post("https://perfanalyticsdb.firebaseio.com/data.json", postData)
-        .then(response => {
-            res.status(200).send(response.data);
-        })
-        .catch( e=> {
-            res.status(404).send(e);
-        })
+    db.push(newData);
+    res.status(200).send(newData)
 })
 
 app.listen(process.env.PORT || 4000, () => {
