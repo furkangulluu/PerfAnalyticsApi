6be185cb7c07cdda07c373971e1f73d0f92804e6
furkangulluu <furkangulluu@gmail.com>
2020-09-14 14:45:28 +0300

    add cors package

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const app = require('express')();
 const bodyParser = require('body-parser');
 const axios = require('axios')
+var cors = require('cors')
+app.use(cors())
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:true}))
